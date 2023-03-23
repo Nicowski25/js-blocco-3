ae883204fdcad1822763f91345972cee9b7b8b45
Nicowski25 <nicola.faedo0610@gmail.com>
2023-03-23 12:28:01 +0100

    variable name

--- snack-2/assets/js/script.js
+++ snack-2/assets/js/script.js
@@ -1,7 +1,8 @@
 //A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
 const parole = ['cane','GaTTo', 'TOPO']
 console.log(parole);
+
 parole.forEach((parola) => {
-    let x = parola.charAt(0).toUpperCase() + parola.slice(1).toLocaleLowerCase();
-    console.log(x);
+    let parolaUppercase = parola.charAt(0).toUpperCase() + parola.slice(1).toLocaleLowerCase();
+    console.log(parolaUppercase);
 })
